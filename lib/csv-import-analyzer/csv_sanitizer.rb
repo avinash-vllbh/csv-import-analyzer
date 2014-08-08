2f3ec60b613d2bdad57c75db0e76618c0b87de1b
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-08-07 19:11:47 -0700

    specs added for csv_sanitizer - failing :)

--- lib/csv-import-analyzer/csv_sanitizer.rb
+++ lib/csv-import-analyzer/csv_sanitizer.rb
@@ -3,6 +3,7 @@ require "tempfile"
 require_relative "analyzer/delimiter_identifier"
 require_relative "helpers/string_class_extensions"
 require_relative "helpers/common_functions"
+require_relative "helpers/errors"
 require_relative "csv_datatype_analysis"
 
 module CsvImportAnalyzer
@@ -14,26 +15,29 @@ module CsvImportAnalyzer
     def process(filename, options)
 
       options = defaults.merge(options)
-      #first thing to do - find the delimiter of the file.
-      delimiter = CsvImportAnalyzer::DelimiterIdentifier.identify_delimiter(filename)
-      options[:delimiter] = delimiter
-      options[:filename] = filename
-
-      File.foreach(filename) do |line|
-        #Check if the line is empty - no point in processing empty lines
-        if line.length > 1
-          line = replace_line_single_quotes(line,delimiter)
-          begin
-            line = CSV.parse_line(line, {:col_sep => delimiter})
-          rescue CSV::MalformedCSVError => error
-            line = "#{line}\""
-            line = CSV.parse_line(line, {:col_sep => delimiter})
+      if File.exist?(filename)
+        options[:filename] = filename
+        #first thing to do - find the delimiter of the file.
+        delimiter = CsvImportAnalyzer::DelimiterIdentifier.identify_delimiter(filename)
+        options[:delimiter] = delimiter
+        File.foreach(filename) do |line|
+          #Check if the line is empty - no point in processing empty lines
+          if line.length > 1
+            line = replace_line_single_quotes(line,delimiter)
+            begin
+              line = CSV.parse_line(line, {:col_sep => delimiter})
+            rescue CSV::MalformedCSVError => error
+              line = "#{line}\""
+              line = CSV.parse_line(line, {:col_sep => delimiter})
+            end
+            line = replace_null_values(line)
           end
-          line = replace_null_values(line)
         end
+        # Cleaned the file - Now analyze for datatypes
+        CsvImportAnalyzer::CsvDatatypeAnalysis.new(options).datatype_analysis
+      else
+        FileNotFound.new
       end
-      # Cleaned the file - Now analyze for datatypes
-      CsvImportAnalyzer::CsvDatatypeAnalysis.new(options).datatype_analysis
     end
 
     private
--- spec/csv-import-analyzer/csv_sanitizer_spec.rb
+++ spec/csv-import-analyzer/csv_sanitizer_spec.rb
@@ -1,6 +1,24 @@
-require 'spec_helper'
+# require 'spec_helper'
 
 describe CsvImportAnalyzer::CsvSanitizer do
-  it ''
+  let (:csv_sanitizer) { CsvImportAnalyzer::CsvSanitizer.new }
+  # let (:test) {["t1","t2","",nil,"t3"]}
+  # let (:res) {["t1","t2","NULL","NULL","t3"]}
+  it 'should handle file not found issue' do
+    expect(csv_sanitizer.process("sample.csv", options = {})).to be_instance_of(FileNotFound)
+  end
+  let (:test) {"\"t1\", 't2', \"t3\""}
+  let (:res) {"\"t1\", \"t2\", \"t3\""}
+  it 'should replace single quotes to double' do
+    
+    expect(csv_sanitizer.send(:replace_line_single_quotes, test, ",")).to eq(res)
+  end
+  context 'testing private methods' do
+    let (:test) {["t1","t2","",nil,"t3"]}
+    let (:res) {["t1","t2","NULL","NULL","t3"]}
+    it 'should replace null values' do
+      expect(csv_sanitizer.send(:replace_null_values, test)).to eq(res)
+    end
+  end
 
 end
