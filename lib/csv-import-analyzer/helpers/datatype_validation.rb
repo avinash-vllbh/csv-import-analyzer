e17ecef41107336df4e2b6d49f84f7b5ae351dbc
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-08-20 18:50:34 -0700

    Added specs for datatype validation

--- lib/csv-import-analyzer/helpers/datatype_validation.rb
+++ lib/csv-import-analyzer/helpers/datatype_validation.rb
@@ -1,55 +1,53 @@
 module CsvImportAnalyzer
-  module Helper
-    class DatatypeValidator
+  module DatatypeValidator
 
-      def validate_field(content)
-        return get_datatype(content)
-      end
+    def validate_field(content)
+      return get_datatype(content)
+    end
 
-      private
-      ###
-      # To check for pattern of Date format after Date.parse is successfull
-      # Date.parse(3000) => true which is not supposed to be true
-      ###
-      def datetime_pattern(field)
-        pattern1 = field.scan(/[0-9]\//)
-        pattern2 = field.scan(/[0-9]\-/)
-        pattern3 = field.scan(/[0-9] /)
-        pattern4 = field.scan(/[0-9] [A-Z][a-z][a-z] [0-9]|[0-9]-[A-Z][a-z][a-z]-[0-9]|[0-9] [a-z][a-z][a-z] [0-9]|[0-9]-[a-z][a-z][a-z]-[0-9]/)
-        if(pattern1.size == 2||pattern2.size == 2||pattern3.size == 2||pattern4.size != 0)
-          return true
-        else
-          return false
-        end
+    private
+    ###
+    # To check for pattern of Date format after Date.parse is successfull
+    # Date.parse(3000) => true which is not supposed to be true
+    ###
+    def datetime_pattern(field)
+      pattern1 = field.scan(/[0-9]\//)
+      pattern2 = field.scan(/[0-9]\-/)
+      pattern3 = field.scan(/[0-9] /)
+      pattern4 = field.scan(/[0-9] [A-Z][a-z][a-z] [0-9]|[0-9]-[A-Z][a-z][a-z]-[0-9]|[0-9] [a-z][a-z][a-z] [0-9]|[0-9]-[a-z][a-z][a-z]-[0-9]/)
+      if(pattern1.size == 2||pattern2.size == 2||pattern3.size == 2||pattern4.size != 0)
+        return true
+      else
+        return false
       end
-      ###
-      #To determine the data-type of an input field
-      ###
-      def get_datatype(field)
-        #Remove if field has any comma's for int and float rep
-        if field != nil && field.class == String
-          num = field.gsub(/,/,'')
-        else
-          num = field
-        end
-        if(Integer(num) rescue false)
-          if num.class == Float
-            return "float"
-          end
-          return "int"
-        elsif(Float(num) rescue false)
+    end
+    ###
+    #To determine the data-type of an input field
+    ###
+    def get_datatype(field)
+      #Remove if field has any comma's for int and float rep
+      if field != nil && field.class == String
+        num = field.gsub(/,/,'')
+      else
+        num = field
+      end
+      if(Integer(num) rescue false)
+        if num.class == Float
           return "float"
-        elsif(Date.parse(field) or Date.strptime(field, '%m/%d/%Y') or Date.strptime(field, '%m-%d-%Y') or Date.strptime(field, '%m %d %Y') rescue false)
-          if datetime_pattern(field)
-            if field =~ /:/ # To check if the field contains any pattern for Hours:minutes
-              return "datetime"
-            else
-              return "date"
-            end
+        end
+        return "int"
+      elsif(Float(num) rescue false)
+        return "float"
+      elsif(Date.parse(field) or Date.strptime(field, '%m/%d/%Y') or Date.strptime(field, '%m-%d-%Y') or Date.strptime(field, '%m %d %Y') rescue false)
+        if datetime_pattern(field)
+          if field =~ /:/ # To check if the field contains any pattern for Hours:minutes
+            return "datetime"
+          else
+            return "date"
           end
         end
-        return "string"
       end
+      return "string"
     end
   end
 end
---
+++ spec/csv-import-analyzer/helpers/datatype_validation_spec.rb
@@ -0,0 +1,61 @@
+# require 'spec_helper'
+require 'date'
+class DummyClass
+end
+
+describe '#validate_field' do
+  
+  before(:each) do
+    @dummy_class = DummyClass.new
+    @dummy_class.extend(CsvImportAnalyzer::DatatypeValidator)
+  end
+
+  context 'knows what an integer looks like' do
+    
+    it 'returns Fixnum type as integer' do
+      expect(@dummy_class.validate_field(10)).to eq("int")
+    end
+    it 'returns Fixnum type with spaces as integer' do
+      expect(@dummy_class.validate_field(' 10 ')).to eq("int")
+    end
+    it 'returns Fixnum type with comma as integer' do
+      expect(@dummy_class.validate_field('1,000')).to eq("int")
+    end
+    it 'returns Fixnum type negative number as integer' do
+      expect(@dummy_class.validate_field(-3)).to eq("int")
+    end
+
+  end
+
+  context 'knows what an Float looks like' do
+    
+    it 'returns Float type as float' do
+      expect(@dummy_class.validate_field(10.0)).to eq("float")
+    end
+    it 'returns Float type with spaces as float' do
+      expect(@dummy_class.validate_field(' 10.01 ')).to eq("float")
+    end
+    it 'returns Float type with comma as float' do
+      expect(@dummy_class.validate_field('1,000.01')).to eq("float")
+    end
+    it 'returns Float type negative number as float' do
+      expect(@dummy_class.validate_field(-3.3)).to eq("float")
+    end
+
+  end
+  
+  context 'it knows what a date looks like' do
+    it 'return true for a valid date type - mm/dd/yyyy' do
+      expect(@dummy_class.validate_field('31/12/2014')).to eq("date")
+    end
+  end 
+
+  context 'it knows what a String looks like' do
+    it 'default to String type' do
+      expect(@dummy_class.validate_field("100 testingNow:)")).to eq("string")
+    end
+    it 'returns String type as string' do
+      expect(@dummy_class.validate_field("Hello")).to eq("string")
+    end
+  end
+end
